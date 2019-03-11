96cabac99d00557861aba1094ade1a829f16d8ef
Shady Abouauf <shadyabouauf@Shadys-MacBook-Pro.local>
2019-03-11 16:08:26 +0200

    imports clean

--- lib/pages/items/items.dart
+++ lib/pages/items/items.dart
@@ -1,16 +1,8 @@
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
-import 'package:groovin_material_icons/groovin_material_icons.dart';
 import 'package:flutter/material.dart';
-import 'package:modal_progress_hud/modal_progress_hud.dart';
 import 'package:n_gen/models/lock.dart';
-import 'package:n_gen/pages/items/item.details.dart';
-import 'package:n_gen/pages/order/order.dart';
-import 'package:n_gen/pages/user/lock_screen.dart';
-import 'package:n_gen/widgets/color_loader_2.dart';
 import 'package:scoped_model/scoped_model.dart';
 import '../../models/item.dart';
-import '../../models/order.dart';
 import '../items/item.card.dart';
 import 'package:n_gen/scoped/connected.dart';
 
